28d890e132fe52914f3e7392f8f616e05c2f6017
Martin Ligtenberg <martin@ligtenberg.digital>
2021-01-20 18:16:19 +0100

    started work on send message page

--- frontend/src/app/messages/queue-message/queue-message.component.ts
+++ frontend/src/app/messages/queue-message/queue-message.component.ts
@@ -5,13 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './queue-message.component.html',
   styleUrls: ['./queue-message.component.scss']
 })
-export class QueueMessageComponent implements OnInit {
-  value: string = '{"aa": "BB"}';
-  editorOptions = { theme: 'vs-light', language: 'json' };
+export class QueueMessageComponent {
+  label: string = '';
+  body: string = '';
+  contentType: string = '';
+
+  editorOptions = { theme: 'vs-light', language: 'text/plain' };
 
   constructor() { }
 
-  ngOnInit(): void {
+  contentTypeUpdated() {
+    this.editorOptions = {...this.editorOptions, language: this.mapContentTypes(this.contentType)}
   }
 
+  private mapContentTypes(contentType: string) {
+    contentType = contentType.toLocaleLowerCase();
+    if (contentType.indexOf('xml') >= 0) {
+      return 'xml';
+    }
+    if (contentType.indexOf('json') >= 0) {
+      return 'json';
+    }
+    if (contentType.indexOf('yaml') >= 0 || contentType.indexOf('yml') >= 0) {
+      return 'yaml';
+    }
+    return 'text/plain';
+  }
+
+  send() {
+    
+  }
 }
