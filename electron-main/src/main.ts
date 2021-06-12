9c0deae6a5ce0214483db6e49b3807692dfe18d0
Martin Ligtenberg <martin@ligtenberg.digital>
2021-06-12 17:55:33 +0200

    fixed error on first start m1

--- electron-main/src/main.ts
+++ electron-main/src/main.ts
@@ -54,22 +54,11 @@ function createWindow(): BrowserWindow | null {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-  global.currentWindow = createWindow();
-});
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   app.quit()
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (global.currentWindow === null) {
-    global.currentWindow = createWindow();
-  }
-})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 if (isDev) {
