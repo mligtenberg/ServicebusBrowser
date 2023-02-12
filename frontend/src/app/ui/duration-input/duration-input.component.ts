65cc2ebce11c4676f972e350344b23e016a708b1
Martin Ligtenberg <martin@ligtenberg.digital>
2023-02-13 00:28:22 +0100

    fix bug where week and days where both filled

--- frontend/src/app/ui/duration-input/duration-input.component.ts
+++ frontend/src/app/ui/duration-input/duration-input.component.ts
@@ -1,89 +1,91 @@
 import { Component, forwardRef, OnDestroy } from '@angular/core';
 import { ControlValueAccessor, UntypedFormBuilder, UntypedFormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
-import * as moment from 'moment';
+import { Duration } from 'luxon';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-duration-input',
-  templateUrl: './duration-input.component.html',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => DurationInputComponent),
-      multi: true
-    }
-  ],
-  styleUrls: ['./duration-input.component.scss']
+    selector: 'app-duration-input',
+    templateUrl: './duration-input.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DurationInputComponent),
+            multi: true,
+        },
+    ],
+    styleUrls: ['./duration-input.component.scss'],
 })
 export class DurationInputComponent implements ControlValueAccessor, OnDestroy {
-  form: UntypedFormGroup;
-  isDisabled: boolean = false;
+    form: UntypedFormGroup;
+    isDisabled: boolean = false;
 
-  private onChange: (_: string) => void;
-  private onTouched: () => void;
-  private _value: moment.Duration;
-  private subs = new Subscription();
-  
-  get value(): moment.Duration {
-    return this._value;
-  }
+    private onChange: (_: string) => void;
+    private onTouched: () => void;
+    private _value: Duration;
+    private subs = new Subscription();
 
-  set value(v: moment.Duration) {
-    this._value = v;
-    if (this.onChange) {
-      this.onChange(v.toISOString());
+    get value(): Duration {
+        return this._value;
     }
-    if (this.onTouched) {
-      this.onTouched();
+
+    set value(v: Duration) {
+        this._value = v;
+        if (this.onChange) {
+            this.onChange(v.toISO());
+        }
+        if (this.onTouched) {
+            this.onTouched();
+        }
     }
-  }
 
-  constructor(formBuilder: UntypedFormBuilder) {
-    this.form = formBuilder.group({
-      seconds: 0,
-      minutes: 0,
-      hours: 0,
-      days: 0,
-      weeks: 0,
-      months: 0,
-      years: 0
-    });
+    constructor(formBuilder: UntypedFormBuilder) {
+        this.form = formBuilder.group({
+            seconds: 0,
+            minutes: 0,
+            hours: 0,
+            days: 0,
+            weeks: 0,
+            months: 0,
+            years: 0,
+        });
 
-    this.subs.add(this.form.valueChanges.subscribe((v) => {
-      this.value = moment.duration(v);
-    }));
-  }
-
-  writeValue(obj: any): void {
-    this._value = moment.duration(obj as string);
-    this.form.setValue({
-      seconds: this._value.seconds(),
-      minutes: this._value.minutes(),
-      hours: this._value.hours(),
-      days: this._value.days(),
-      weeks: this._value.weeks(),
-      months: this._value.months(),
-      years: this._value.years()
-    });
-  }
-
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  setDisabledState?(isDisabled: boolean): void {
-    if(isDisabled) {
-      this.form.disable();
-    } else {
-      this.form.enable();
+        this.subs.add(
+            this.form.valueChanges.subscribe((v) => {
+                this.value = Duration.fromObject(v);
+            })
+        );
     }
-  }
 
-  ngOnDestroy() {
-    this.subs.unsubscribe();
-  }
+    writeValue(obj: any): void {
+        this._value = Duration.fromISO(obj as string);
+        this.form.setValue({
+            seconds: this._value.seconds,
+            minutes: this._value.minutes,
+            hours: this._value.hours,
+            days: this._value.days,
+            weeks: this._value.weeks,
+            months: this._value.months,
+            years: this._value.years,
+        });
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        if (isDisabled) {
+            this.form.disable();
+        } else {
+            this.form.enable();
+        }
+    }
+
+    ngOnDestroy() {
+        this.subs.unsubscribe();
+    }
 }
