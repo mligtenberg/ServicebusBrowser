110fd4f26554be8bd7107a445e3eb08ff184956d
Martin Ligtenberg <martin@ligtenberg.digital>
2025-01-11 23:36:51 +0100

    block reloading page

--- apps/servicebus-browser-frontend/src/main.ts
+++ apps/servicebus-browser-frontend/src/main.ts
@@ -5,3 +5,8 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+window.addEventListener('beforeunload', (e) => {
+  // Prevent the default behavior of reloading
+  e.preventDefault();
+});
