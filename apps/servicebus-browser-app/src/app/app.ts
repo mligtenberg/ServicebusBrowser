308fe95b4c8f8b49c21ed2b053adbc7d943eef7e
Martin Ligtenberg <martin@ligtenberg.digital>
2025-01-12 23:46:59 +0100

    some fixes for development mode

--- apps/servicebus-browser-app/src/app/app.ts
+++ apps/servicebus-browser-app/src/app/app.ts
@@ -3,7 +3,7 @@ import { rendererAppName, rendererAppPort } from './constants';
 import { environment } from '../environments/environment';
 import { join } from 'path';
 import { format } from 'url';
-import { menu } from './menu';
+import { getMenu } from './menu';
 
 export default class App {
   // Keep a global reference of the window object, if you don't, the window will
@@ -80,7 +80,7 @@ export default class App {
 
     // if main window is ready to show, close the splash window and show the main window
     App.mainWindow.once('ready-to-show', () => {
-      Menu.setApplicationMenu(menu);
+      Menu.setApplicationMenu(getMenu((App.isDevelopmentMode())));
       App.mainWindow.show();
     });
 
--- apps/servicebus-browser-app/src/app/menu.ts
+++ apps/servicebus-browser-app/src/app/menu.ts
@@ -1,40 +1,53 @@
 import { app, Menu, MenuItemConstructorOptions, MenuItem } from 'electron'
 
-const isMac = process.platform === 'darwin';
+export function getMenu(isDev: boolean) {
+  const isMac = process.platform === 'darwin';
 
-const macTemplate: Array<MenuItemConstructorOptions | MenuItem> = [
-  // { role: 'appMenu' }
-  {
-    label: app.name,
-    submenu: [
-      {
-        label: 'About ' + app.name,
-        role: 'about'
-      },
-      { type: 'separator' },
-      {
-        label: 'Quit ' + app.name,
-        role: 'quit'
-      }
-    ]
-  },
-  // { role: 'fileMenu' }
-  {
-    label: 'File',
-    submenu: [
-      { role: 'close' }
-    ]
-  }
-];
+  const macTemplate: Array<MenuItemConstructorOptions | MenuItem> = [
+    // { role: 'appMenu' }
+    {
+      label: app.name,
+      submenu: [
+        {
+          label: 'About ' + app.name,
+          role: 'about'
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit ' + app.name,
+          role: 'quit'
+        },
+        ...(isDev ? [
+          {
+            type: 'separator'
+          } as MenuItemConstructorOptions,
+          {
+            role: 'reload'
+          } as MenuItemConstructorOptions,
+          {
+            role: 'toggleDevTools'
+          } as MenuItemConstructorOptions
+        ] : [])
+      ]
+    },
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        { role: 'close' }
+      ]
+    }
+  ];
 
-const winLinTemplate: Array<MenuItemConstructorOptions | MenuItem> = [
-  // { role: 'fileMenu' }
-  {
-    label: 'File',
-    submenu: [
-      { role: 'quit' }
-    ]
-  }
-];
+  const winLinTemplate: Array<MenuItemConstructorOptions | MenuItem> = [
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        { role: 'quit' }
+      ]
+    }
+  ];
 
-export const menu = Menu.buildFromTemplate(isMac ? macTemplate : winLinTemplate);
+  return Menu.buildFromTemplate(isMac ? macTemplate : winLinTemplate);
+}
