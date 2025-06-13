f596886faf3d25b2e398424760695a569fba1e81
Martin Ligtenberg <martin@ligtenberg.digital>
2025-06-13 21:57:00 +0200

    small fix

--- apps/servicebus-browser-frontend/src/main.ts
+++ apps/servicebus-browser-frontend/src/main.ts
@@ -3,9 +3,6 @@ import { Router } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .then((appRef) => {
-    const router = appRef.injector.get(Router);
-    router.preload();
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
