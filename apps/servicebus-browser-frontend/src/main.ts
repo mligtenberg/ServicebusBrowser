e67a861b607f57d0b8ee6fdf017dcbac7d74cdfe
Martin Ligtenberg <martin@ligtenberg.digital>
2025-06-13 21:59:13 +0200

    remove old dependecy

--- apps/servicebus-browser-frontend/src/main.ts
+++ apps/servicebus-browser-frontend/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
