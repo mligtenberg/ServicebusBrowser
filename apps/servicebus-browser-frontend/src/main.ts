de431dfcff9a798348bcc89210f2b10bb96a337e
Martin Ligtenberg <martin@ligtenberg.digital>
2025-06-13 21:55:05 +0200

    feat(frontend): enable aggressive preloading

--- apps/servicebus-browser-frontend/src/main.ts
+++ apps/servicebus-browser-frontend/src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, appConfig)
+  .then((appRef) => {
+    const router = appRef.injector.get(Router);
+    router.preload();
+  })
+  .catch((err) => console.error(err));
