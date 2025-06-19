acf3ee9678a7ce0d2bfaa0e507e8a19c6a44cd2c
Martin Ligtenberg <martin@ligtenberg.digital>
2025-06-19 22:05:59 +0200

    test(messages-store): add effects tests

---
+++ libs/messages/store/src/lib/messages-tasks.effects.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { ReplaySubject, firstValueFrom } from 'rxjs';
+import { MessagesTasksEffects } from './messages-tasks.effects';
+import * as internalActions from './messages.internal-actions';
+import { ReceiveEndpoint } from '@service-bus-browser/service-bus-contracts';
+import { TasksActions } from '@service-bus-browser/tasks-store';
+
+const endpoint: ReceiveEndpoint = { connectionId: '00000000-0000-0000-0000-000000000001', queueName: 'q', channel: undefined };
+
+describe('MessagesTasksEffects', () => {
+  let actions$: ReplaySubject<any>;
+  let effects: MessagesTasksEffects;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MessagesTasksEffects, provideMockActions(() => actions$)]
+    });
+    effects = TestBed.inject(MessagesTasksEffects);
+  });
+
+  it('creates task on load', async () => {
+    actions$ = new ReplaySubject(1);
+    actions$.next(internalActions.peekMessagesLoad({ pageId: '00000000-0000-0000-0000-000000000002', endpoint, maxAmount: 5, alreadyLoadedAmount: 0, fromSequenceNumber: '0' }));
+    const result = await firstValueFrom(effects.createTask$);
+    expect(result).toEqual(TasksActions.createTask({
+      id: '00000000-0000-0000-0000-000000000002',
+      statusDescription: '0/5',
+      description: 'loading messages from q',
+      hasProgress: true,
+      initialProgress: 0
+    }));
+  });
+});
