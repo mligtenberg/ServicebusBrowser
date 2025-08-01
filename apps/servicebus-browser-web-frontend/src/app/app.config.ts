157a28874cc65b3d8e064854e484a71a0c1e3269
Martin Ligtenberg <martin@ligtenberg.digital>
2025-08-01 21:00:53 +0200

    make it run

--- apps/servicebus-browser-web-frontend/src/app/app.config.ts
+++ apps/servicebus-browser-web-frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -9,7 +9,7 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };
