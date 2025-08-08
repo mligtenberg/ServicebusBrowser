a956cb9384cc46ad2f88fcdecf58e7580dfe447c
Martin Ligtenberg <martin@ligtenberg.digital>
2025-08-08 09:43:17 +0200

    fix for azure web apps, port of backend is static, use nginx to properly bind

--- apps/servicebus-browser-web-backend/src/main.ts
+++ apps/servicebus-browser-web-backend/src/main.ts
@@ -68,7 +68,7 @@ app.post('/api/management/command', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3333;
+const port = 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
