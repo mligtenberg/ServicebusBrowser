bf4e5db5d40aeb476b202803e1c228b73b0e3b6c
Martin Ligtenberg <martin@ligtenberg.digital>
2026-02-24 22:30:19 +0100

    properly persist opfs

--- apps/servicebus-browser-frontend/src/main.ts
+++ apps/servicebus-browser-frontend/src/main.ts
@@ -2,6 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+if (navigator.storage && navigator.storage.persist) {
+  const persistent = await navigator.storage.persist();
+
+  if (persistent) {
+    console.log('Storage will not be cleared except by explicit user action');
+  } else {
+    console.log('Storage may be cleared by the UA under storage pressure.');
+  }
+}
+
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
--- apps/servicebus-browser-web-frontend/src/main.ts
+++ apps/servicebus-browser-web-frontend/src/main.ts
@@ -3,6 +3,16 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { initializeClientConfig } from './app/client-config-loader';
 
+if (navigator.storage && navigator.storage.persist) {
+  const persistent = await navigator.storage.persist();
+
+  if (persistent) {
+    console.log('Storage will not be cleared except by explicit user action');
+  } else {
+    console.log('Storage may be cleared by the UA under storage pressure.');
+  }
+}
+
 initializeClientConfig().then(() => {
   bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 });
